3f1c916e6707b40d07442e2967936e05994be848
Dejan Blagojevity <bv@kingshigh.school.nz>
2024-08-21 11:49:23 +1200

    1.1. Simple Movement

---
+++ scenes/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var speed=50
+
+func get_input():
+	var input_direction=Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity=input_direction*speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()	
+	
+	
+	
